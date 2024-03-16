01d4e64dabc61a963879279f22cccafac2237453
KiranDev12 <kirandev.edu@gmail.com>
2024-03-16 10:53:40 +0530

    fixed server side rendering issue

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -10,7 +10,6 @@ interface CanvasProps {
 }
 export const Canvas = ({ boardId }: CanvasProps) => {
   const info = useSelf((me) => me.info);
-  console.log(info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info />
--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
@@ -6,8 +8,8 @@ export const Info = () => {
   );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
   return (
-    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"/>
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
   );
 };
