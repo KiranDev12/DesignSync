fd08e07d22193e2859ec7b2f5ad178d739922eca
KiranDev12 <kirandev.edu@gmail.com>
2024-03-17 11:16:48 +0530

    canvas state and layer types defined for all tools

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,6 +1,13 @@
 "use client";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
-import { useSelf } from "@/liveblocks.config";
+import {
+  useHistory,
+  useSelf,
+  useCanRedo,
+  useCanUndo,
+} from "@/liveblocks.config";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
@@ -9,12 +16,26 @@ interface CanvasProps {
   boardId: string;
 }
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   const info = useSelf((me) => me.info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
