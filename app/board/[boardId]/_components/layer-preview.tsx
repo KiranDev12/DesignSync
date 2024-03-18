e09b59311f32fc54773cc7978c90f2d43ebb069d
KiranDev12 <kirandev.edu@gmail.com>
2024-03-18 16:05:13 +0530

    other elements than rectangle added

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -4,9 +4,10 @@ import { memo } from "react";
 
 import { LayerType } from "@/types/canvas";
 import { useStorage } from "@/liveblocks.config";
-
+import { Text } from "./text";
+import { Ellipse } from "./ellipse";
 import { Rectangle } from "./rectangle";
-
+import { Note } from "./note";
 interface LayerPreviewProps {
   id: string;
   onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
@@ -22,6 +23,33 @@ export const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <Note
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Rectangle:
         return (
           <Rectangle
